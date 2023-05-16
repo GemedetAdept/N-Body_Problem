fbbf9c1240cec726e67416e39867645453424909
Vivian Zitek <vivian.zitek@gmail.com>
2023-05-16 19:22:01 -0400

    Clean up main program.

--- src/main.c
+++ src/main.c
@@ -9,52 +9,14 @@ void main() {
 
 	// mass (kg), distance (m), time (s) = force (N)
 	double const gravitational_constant = 6.674 * (pow(10.0, -11.0));
-	double force, massA, massB, center_dist, accelMassA, accelMassB, barycenter;
 
 	int bodyCount = 2;
 
-	
-	
-	MassBody MassBodyArray[bodyCount];
-	int sizeMBArray = sizeof(MassBodyArray)/32;
-	double RelativePointDists[bodyCount][bodyCount];
-
-	// massA = 500.0;
-	// massB = 200.0;
-	// center_dist = 1.0;
-
-	// force = getGravitation(gravitational_constant, massA, massB, center_dist);
-	// accelMassA = getAcceleration(force, massA);
-	// accelMassB = getAcceleration(force, massB);
-	// barycenter = getBarycenter(massA, massB, center_dist);
-
-	// printf("%s\n\n", "|| Newton's Law of Universal Gravitation ||");
-
-	// printParameters(massA, massB, center_dist);
-	// printBarycenters(center_dist, barycenter);
-	// printForce(force);
-	// printAcceleration(accelMassA, accelMassB);
-	// fflush(stdout);
+	BodyArray[0].Type = isLength;
+	BodyArray[0].Value.Length = bodyCount;
 
 	MassBody bodyA = {500.0, 0.0, 1.0, 1.0};
 	MassBody bodyB = {200.0, 0.0, 5.0, 7.0};
-
-	MassBodyArray[0] = bodyA;
-	MassBodyArray[1] = bodyB;
-
-	double pointDist = getPointDistance(&bodyA, &bodyB);
-	printf("%f%s\n", pointDist, " | Dist between bodyA & bodyB");
-
-	// fillRelativeDistArray((double *)RelativePointDists, MassBodyArray, sizeMBArray);
-
-	printMassBody(&MassBodyArray[0]);
-	printMassBody(&MassBodyArray[1]);
-	fflush(stdout);
-
-	// -------------------------------------------------------------------------------
-
-	BodyArray[0].Type = isLength;
-	BodyArray[0].Value.Length = 9;
 
 	BodyArray[1].Type = isMassBody;
 	BodyArray[1].Value.BodyNumber = 1;
@@ -64,6 +26,12 @@ void main() {
 	BodyArray[2].Value.BodyNumber = 2;
 	BodyArray[2].Value.Body = bodyB;
 
+	double RelativePointDists[bodyCount][bodyCount];
+
+	double pointDist = getPointDistance(&bodyA, &bodyB);
+	printf("%f%s\n", pointDist, " | Dist between bodyA & bodyB");
+
+	printMassBody(&BodyArray[1].Value.Body);
 	printMassBody(&BodyArray[2].Value.Body);
 	fflush(stdout);
 }
